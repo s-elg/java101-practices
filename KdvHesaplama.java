e0b0975bd8364cbc71c24ab57c4aae8c407cdef9
sümeyye elgin <115994927+s-elg@users.noreply.github.com>
2023-10-12 15:10:58 +0300

    Add files via upload

---
+++ KdvHesaplama.java
@@ -0,0 +1,23 @@
+package pratikler;
+
+import java.util.Scanner;
+public class KdvHesaplama {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        double tutar, kdvTutari, kdvOrani = 0.18;
+        int fiyat;
+
+        System.out.print("Lütfen alacağınız ürünün fiyatını girinz: ");
+        fiyat = input.nextInt();
+
+        kdvTutari = fiyat * kdvOrani;
+
+        tutar = kdvTutari + fiyat;
+        System.out.println("Almayı seçtiğiniz ürünün KDV'siz fiyatı: " + fiyat);
+        System.out.println("Aldığınız ürünün KDV tutarı: " + kdvTutari);
+        System.out.println("Aldığınız ürünün KDV eklenmiş fiyatı: " + tutar);
+
+
+
+    }
+}
